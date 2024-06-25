9a961397c18c38ede8090d48eba3a7db499896d5
Ayushi Thakur <96823295+Ayushithakur-2003@users.noreply.github.com>
2024-06-25 21:31:42 +0530

    Create index.jsx

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,9 @@
+const NavBar = () =>{
+    return(
+        <div class="fixed top-0 left-0 w-screen h-fit p-6 bg-zinc-900">
+        <h1 className=" text-white text-3xl">Virtual Museum of Space Exploration</h1>
+        </div>
+    )
+}
+
+export default NavBar;
